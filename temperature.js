2b020279d9efd2492affc55e347ede33d60f3664
glahmann <lahmanng@gmail.com>
2016-10-09 12:42:48 -0700

    fix temperature

--- temperature.js
+++ temperature.js
@@ -6,8 +6,7 @@ var board = new five.Board({
   io: new Tessel()
 });
 
-board.on('ready', function() {
-  // Write your program locally and push to the Tessel 2 when ready!
+board.on('ready', function(){
   var fahrenheitGraph = new Barcli({
     label: 'Fahrenheit',
     range: [20, 120]
@@ -23,38 +22,19 @@ board.on('ready', function() {
     range: [250, 325]
   })
 
-  board.on('ready', function(){
-    var temp = new Tessel.Temperature('A0');
-
-    temp.on('data', function(err, data){
-      fahrenheitGraph.update(data.fahrenheit);
-      celsiusGraph.update(data.celsius);
-      kelvinGraph.update(data.Kelvin);
-    });
-});
-/*
-var fahrenheitGraph = new Barcli({
-  label: 'Fahrenheit',
-  range: [20, 120]
-});
-
-var celsiusGraph = new Barcli({
-  label: 'Celsius',
-  range: [6, 50]
-});
-
-var kelvinGraph = new Barcli({
-  label: 'Kelvin',
-  range: [250, 325]
-})
-
-board.on('ready', function(){
-  var temp = new tessel.Temperature('A0');
-
-  temp.on('data', function(err, data){
-    fahrenheitGraph.update(data.fahrenheit);
-    celsiusGraph.update(data.celsius);
-    kelvinGraph.update(data.Kelvin);
+  var temperature = new five.Temperature({
+    controller: "TMP36",
+    pin: "A4",
+    aref: 3.3
   });
-})
-*/
+
+  temperature.on('data', function(err, data){
+    console.log(this.raw);
+    console.log(err);
+    console.log(this.celsius + "°C", this.fahrenheit + "°F");
+    console.log(data);
+    fahrenheitGraph.update(this.fahrenheit); //Should use data.fahrenheit
+    celsiusGraph.update(this.celsius);
+    kelvinGraph.update(this.kelvin);
+  });
+});
